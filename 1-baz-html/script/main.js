223ed55b71868dc60ad24cfeefd4105df589be9a
Baton1488 <dimidrol.fakof@yandex.ru>
2025-04-07 22:37:51 +0400

    Ну типа 2

--- 1-baz-html/script/main.js
+++ 1-baz-html/script/main.js
@@ -1,2 +1,26 @@
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/brat.png") {
+    myImage.setAttribute("src", "images/orig.jpg");
+  } else {
+    myImage.setAttribute("src", "images/brat.png");
+  }
+};
+var myButton = document.querySelector("button");
 var myHeading = document.querySelector("h1");
-myHeading.textContent = "Хочешь постою как цапля. КУРЛЫК, КУРЛЫК, КУРЛЫК, КУРЛЫК";
+function setUserName() {
+    var myName = prompt("Ты кто такой?.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = myName +" ,меня доебали твои охуительные истории";
+  }
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = myName +" ,меня доебали твои охуительные истории";
+}  
+myButton.onclick = function () {
+    setUserName();
+  };
